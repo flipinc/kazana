16f22c76c7e928596b7ca584cc8253bbeeac27e8
Keisuke Iizuka <26keisuke@MacBook-Pro.local>
2021-03-07 23:18:06 +0900

    ✨ added distribution flow

--- src/main/websocket.ts
+++ src/main/websocket.ts
@@ -8,88 +8,103 @@ interface CustomWebsocket extends Websocket {
   pingTimeout?: number;
 }
 
-let socket: CustomWebsocket | null = null;
+/**
+ * Websocket connection to KIKI
+ *
+ * TODO: if connection to kiki fails, retry connection every n seconds
+ */
+export default class Socket {
+  socket: CustomWebsocket | null;
 
-const makeWsConnection = ({
-  signature,
-  userId,
-  orgId,
-}: {
-  signature: string;
-  userId: string;
-  orgId: string;
-}) => {
-  socket = new Websocket(
-    `http://localhost:4000/kiki/?type=kazana&signature=${signature}&userId=${userId}&orgId=${orgId}`,
-  );
+  callbacks: { [action in KIKIAction]: ((payload: any) => void) | null };
 
-  const heartbeat = () => {
-    if (!socket) return; // if previous heartbeat fails, socket will be none
+  constructor() {
+    this.socket = null;
+    this.callbacks = {
+      message: null,
+    };
+  }
 
-    clearTimeout(socket.pingTimeout);
-    socket.pingTimeout = (setTimeout(() => {
-      if (!socket) return;
-      if (!process.send) return process.exit();
+  /**
+   * Register callbacks exposed
+   * @param action
+   * @param callback
+   */
+  onMessage(callback: (payload: KIKIMessage) => void) {
+    this.callbacks.message = callback;
+  }
 
-      process.send(JSON.stringify({ action: "kiki-disconnected" }));
-      socket.close();
-      socket = null;
-    }, 30000 + 1000) as unknown) as number;
-  };
-
-  socket.onopen = () => {
-    if (!process.send) return process.exit();
-
-    process.send(JSON.stringify({ action: "kiki-connected" }));
-    heartbeat();
-  };
-
-  socket.onclose = () => {
-    if (!socket) return;
-
-    clearTimeout(socket.pingTimeout);
-    socket = null;
-  };
-
-  socket.onmessage = (message) => {
-    if (!socket) return;
-    if (!process.send) return process.exit();
-
-    const msg = JSON.parse(message.data.toString());
-    switch (msg.action) {
-      case "ping":
-        heartbeat();
-        socket.send(JSON.stringify({ action: "pong" }));
-        break;
-      case "start-talk":
-        process.send(message.data);
-        break;
-      case "end-talk":
-        process.send(message.data);
+  private execute(action: KIKIAction, message?: KIKIMessage) {
+    switch (action) {
+      case "message":
+        if (!this.callbacks.message) {
+          throw Error("Callback for `message` is not registered.");
+        }
+        this.callbacks.message(message);
         break;
       default:
-        process.send(message.data);
         break;
     }
-  };
-};
-
-process.on("message", (message) => {
-  const msg = JSON.parse(message);
-
-  console.log(msg);
-
-  switch (msg.action) {
-    case "login":
-      makeWsConnection(msg.payload);
-      break;
-    case "logout":
-      if (socket) {
-        socket.close();
-        socket = null;
-      }
-      break;
-    default:
-      break;
   }
-});
+
+  heartbeat() {
+    if (!this.socket) return; // if previous heartbeat fails, socket will be none
+
+    clearTimeout(this.socket.pingTimeout);
+    this.socket.pingTimeout = (setTimeout(() => {
+      if (!this.socket) return;
+
+      this.execute("message", { action: "kiki-disconnected" });
+      this.socket.close();
+      this.socket = null;
+    }, 30000 + 1000) as unknown) as number;
+  }
+
+  connect({
+    signature,
+    userId,
+    orgId,
+  }: {
+    signature: string;
+    userId: string;
+    orgId: string;
+  }) {
+    this.socket = new Websocket(
+      `http://localhost:4000/kiki/?type=kazana&signature=${signature}&userId=${userId}&orgId=${orgId}`,
+    );
+
+    this.socket.onopen = () => {
+      this.execute("message", { action: "kiki-connected" });
+      this.heartbeat();
+    };
+
+    this.socket.onclose = () => {
+      if (!this.socket) return;
+
+      clearTimeout(this.socket.pingTimeout);
+      this.socket = null;
+    };
+
+    this.socket.onmessage = (message) => {
+      if (!this.socket) return;
+
+      const msg: KIKIMessage = JSON.parse(message.data.toString());
+      switch (msg.action) {
+        case "ping":
+          this.heartbeat();
+          this.socket.send(JSON.stringify({ action: "pong" }));
+          break;
+        default:
+          this.execute("message", msg);
+          break;
+      }
+    };
+  }
+
+  close() {
+    if (this.socket) {
+      this.socket.close();
+      this.socket = null;
+    }
+  }
+}
