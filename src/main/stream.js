a8384c595a8b006a2e0ff1b5b609ddd51f39eafe
keisuke-iizuka <keisuke.iizuka@flip.inc>
2021-03-16 21:25:20 +0900

    ✨ built tflite with flexdelegate

--- src/main/stream.js
+++ src/main/stream.js
@@ -1,8 +1,8 @@
 const addon = require("../../build/Release/wormhole")
 
-const encoderType = 0;
-const chunkSize = 20480
-const rightSize = 5120
+const encoderType = 0; // emformer
+const chunkSize = 20480 // 16000 (sample_rate) x 1.28
+const rightSize = 5120 // 16000 (sample_rate) x 0.32
 
 const Stream = new addon.Stream(encoderType, chunkSize, rightSize, 4);
 
