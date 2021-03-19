da497e57d493dc9376e44821c52081308922e493
Keisuke Iizuka <26keisuke@MacBook-Pro.local>
2021-03-19 12:14:14 +0900

    🐛 changed wormhole->/build electron->/out

--- src/main/stream.js
+++ src/main/stream.js
@@ -1,4 +1,4 @@
-const addon = require("../../out/Release/wormhole.node")
+const addon = require("bindings")("wormhole");
 
 const encoderType = 0; // emformer
 const chunkSize = 20480; // 16000 (sample_rate) x 1.28
@@ -8,14 +8,14 @@ const stream = new addon.Stream(encoderType, chunkSize, rightSize);
 
 stream.getDevices();
 stream.onBundle((buffer) => {
-    console.log(buffer.byteLength)
-    // https://stackoverflow.com/questions/49132819/reading-raw-bytes-from-a-c-float-array-in-javascript-conversion-to-js-array
-    console.log(buffer.readFloatLE())
-    // pythonに送る流れ
-    // https://stackoverflow.com/questions/37330746/javascript-smallest-json-stringify-for-float32array
-    // https://stackoverflow.com/questions/57367036/pass-a-pickle-buffer-from-node-to-python
-})
+  console.log(buffer.byteLength);
+  // https://stackoverflow.com/questions/49132819/reading-raw-bytes-from-a-c-float-array-in-javascript-conversion-to-js-array
+  console.log(buffer.readFloatLE());
+  // pythonに送る流れ
+  // https://stackoverflow.com/questions/37330746/javascript-smallest-json-stringify-for-float32array
+  // https://stackoverflow.com/questions/57367036/pass-a-pickle-buffer-from-node-to-python
+});
 
 stream.start();
 
-module.exports = stream
+module.exports = stream;
